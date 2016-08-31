7489211b8fdfa01beefbcdf6e2f2c565d3d3f069
team11 <jiangtaiyang@team11.cn>
2016-08-31 17:21:28 +0800

    add mybatis generator.

---
+++ src/test/java/com/bestxty/ds/JdbcTest.java
@@ -0,0 +1,20 @@
+package com.bestxty.ds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by xty on 2016/8/31.
+ * for project: DistributedSystemTest.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-config.xml"})
+public class JdbcTest {
+
+    @Test
+    public void testJdbc() {
+
+    }
+}

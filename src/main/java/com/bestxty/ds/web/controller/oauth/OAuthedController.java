68dcbb1bb0ed5fd2dc97e747062197a12b9824b3
team11 <jiangtaiyang@team11.cn>
2016-09-06 20:31:41 +0800

    update oauth2.0 config.

--- src/main/java/com/bestxty/ds/web/controller/oauth/OAuthedController.java
+++ src/main/java/com/bestxty/ds/web/controller/oauth/OAuthedController.java
@@ -2,6 +2,7 @@ package com.bestxty.ds.web.controller.oauth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by xty on 2016/9/4.
@@ -10,4 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/oauthed")
 public class OAuthedController {
+
+
+    @RequestMapping("/res")
+    @ResponseBody
+    public String oauthResrouce() {
+        return "oauthResrouce";
+    }
 }
